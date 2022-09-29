5f01c4a08a18137bb524c5bb82b47b89759043ce
Stachacl <stacha.l@icloud.com>
2022-09-30 08:49:44 +1000

    created footer component, styles file

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,26 +1,34 @@
-import React from 'react'
-import './VideoStyles.css'
-import { Link } from 'react-router-dom'
-import spaceVideo from '../assets/space.mp4'
+import React from "react";
+import "./VideoStyles.css";
+import { Link } from "react-router-dom";
+import spaceVideo from "../assets/space.mp4";
 
 const Video = () => {
   return (
-    <div className='hero'>
-        <video autoPlay loop muted id='video'>
-            <source src={spaceVideo} type="video/mp4" />
-        </video>
+    <div className="hero">
+      <video autoPlay loop muted id="video">
+        <source src={spaceVideo} type="video/mp4" />
+      </video>
 
-        <div className='content'> 
-           <h1> Space: the final frontier</h1>
-           <p> It's a five-year mission to explore strange new worlds, <br /> to seek our new life and new civilizations, to boldly go where no man has gone before.</p>
-        </div>
-        
+      <div className="content">
+        <h1> Space: the final frontier</h1>
+        <p>
+          It's a five-year mission to explore strange new worlds, <br /> to seek
+          our new life and new civilizations, to boldly go where no man has gone
+          before.
+        </p>
+
         <div>
-          <Link to="/pricing" className='btn'>Pricing</Link>
-          <Link to="/contact" className='btn btn-light'>Launch</Link>
+          <Link to="/pricing" className="btn">
+            Pricing
+          </Link>
+          <Link to="/contact" className="btn btn-light bg-opacity-10">
+            Launch
+          </Link>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Video from "../components/Video";
 
+
 const Home = () => {
   return (
     <div>
