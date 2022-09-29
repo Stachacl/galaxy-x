2f6b01a21a7cc49213c2957b40bb2b4ee67c8e25
Stachacl <stacha.l@icloud.com>
2022-09-29 13:36:18 +1000

    nav menu appears on click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,18 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./NavbarStyles.css";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-
-    const [click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
 
   return (
     <div className="header">
       <Link to="/">
         <h1>GALAXY-X</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={ click ? "nav-menu active" : "nav-menu"}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -27,10 +26,12 @@ const Navbar = () => {
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
-      <div className="hamburger" 
-       onClick={handleClick}>
-       { click ? (<FaTimes/>) : (<FaBars size={20} style={{ color: "whitesmoke" }} />)}
-        
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "whitesmoke" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "whitesmoke" }} />
+        )}
       </div>
     </div>
   );
