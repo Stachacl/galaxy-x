ee1f2983ee207d2e425c332ddc60e9b9367b0b63
Stachacl <stacha.l@icloud.com>
2022-09-30 10:28:12 +1000

    filling the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaTwitter, FaGithub, FaLinkedin, FaPhone, FaSearchLocation  } from 'react-icons/fa'
+import { FaTwitter, FaGithub, FaLinkedin, FaPhone, FaSearchLocation, FaMailBulk } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 
@@ -9,12 +9,43 @@ const Footer = () => {
         <div className='footer-container'>
             <div className='left'>
                 <div className='location'>
-                    <Link className='location-icon'> 
+                    <div className='footer-icon'> 
                     <FaSearchLocation />
-                    </Link>
+                    </div>
+                    <div>
+                    <p> 10700 Escondido Canyon Rd, Agua Dulce, CA 91390, United States
+                    </p>
+                    </div>
+                </div>
+                <div className='phone'>
+                     <div className='footer-icon'> 
+                    <FaPhone />
+                    </div>
+                    <p>  +1 661-268-0840 </p>
+                </div>
+
+                <div className='email'>
+                     <div className='footer-icon'> 
+                    <FaMailBulk />
+                    </div>
+                    <p> tiberius@space-x.com </p>
                 </div>
 
             </div>
+
+
+            <div className='right'>
+             
+                 <p> Join our award winning team and follow us on social media.
+                 </p>
+        
+                 <div className='footer-icon right'> 
+                    <FaGithub className='footer-icon right' />
+                    <FaTwitter className='footer-icon right'/>
+                    <FaLinkedin className='footer-icon right'/>
+                    </div>
+                 </div>
+            
         </div>
     </div>
   )
