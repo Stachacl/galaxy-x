a5df43298c377ed998664bab6060ba12b8c27bcf
Stachacl <stacha.l@icloud.com>
2022-10-11 18:17:39 +1100

    pricing page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./routes/Home";
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Contact from './routes/Contact';
+
+import Home from './routes/Home'
+import Pricing from './routes/Pricing'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/contact' element={<Contact />} />
+
       </Routes>
     </>
   );
