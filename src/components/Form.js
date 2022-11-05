edc58c09f450988842caee8968ccc5c0b0e66bcb
Stachacl <stacha.l@icloud.com>
2022-11-06 10:18:21 +1100

    moved diclaimer to the form - just before the submit button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './FormStyles.css'
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
+
 
 const Form = () => {
     return (
@@ -13,6 +15,16 @@ const Form = () => {
                 <input type='text'></input>
                 <label>Details</label>
                 <textarea rows='6' placeholder='Type a short message here' />
+
+                <div className='container center footer-icon'> 
+                   <p> Disclaimer: If you wish to contact Stacha - please connect via social media listed below.</p>
+                </div>
+                <div className='container center footer-icon pb2'> 
+                   <a href=" https://github.com/Stachacl/galaxy-x"> <FaGithub className='footer-icon right center' /> </a> 
+                   <a href="https://twitter.com/Stacha_cl">  <FaTwitter className='footer-icon right center'/>  </a> 
+                   <a href="https://www.linkedin.com/in/stacha-c-a7b3b51bb/">  <FaLinkedin className='footer-icon  center'/> </a> 
+                </div>
+
                 <button className='btn'>Submit</button>
             </form>
         </div>
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,7 +3,7 @@ import Footer from '../components/Footer'
 import HeroImage from '../components/HeroImage'
 import Navbar from '../components/Navbar'
 import Form from '../components/Form'
-import { FaTwitter, FaGithub, FaLinkedin, FaPhone, FaSearchLocation, FaMailBulk } from 'react-icons/fa'
+
 
 const Contact = () => {
     return (
@@ -12,14 +12,6 @@ const Contact = () => {
             <HeroImage heading='CONTACT GALAXY X' text='This website was created by Stacha Clemens.'> 
             </HeroImage>
             <Form />
-            <div className='container center footer-icon'> 
-                   <p> Disclaimer: If you wish to contact Stacha - please connect via social media listed below.</p>
-            </div>
-            <div className='container center footer-icon'> 
-                   <a href=" https://github.com/Stachacl/galaxy-x"> <FaGithub className='footer-icon right' /> </a> 
-                   <a href="https://twitter.com/Stacha_cl">  <FaTwitter className='footer-icon right'/>  </a> 
-                   <a href="https://www.linkedin.com/in/stacha-c-a7b3b51bb/">  <FaLinkedin className='footer-icon right'/> </a> 
-                </div>
             <Footer/>
         </div>
     )
