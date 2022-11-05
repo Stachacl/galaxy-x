41fb1d370708aa8dca5fca676ce08ffeb8831940
Stachacl <stacha.l@icloud.com>
2022-11-06 10:11:53 +1100

    contact page disclaimer added

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -9,19 +9,17 @@ const Contact = () => {
     return (
         <div>
             <Navbar />
-            <HeroImage heading='CONTACT GALAXY X' text='This website was created by Stacha Clemens. If you wish to contact her - please connect via Twitter' />
-            <div>
-                 <p> This website was created by Stacha Clemens. If you wish to contact her - please connect via social media. 
-                 </p>
-        
-                 <div className='container center footer-icon'> 
+            <HeroImage heading='CONTACT GALAXY X' text='This website was created by Stacha Clemens.'> 
+            </HeroImage>
+            <Form />
+            <div className='container center footer-icon'> 
+                   <p> Disclaimer: If you wish to contact Stacha - please connect via social media listed below.</p>
+            </div>
+            <div className='container center footer-icon'> 
                    <a href=" https://github.com/Stachacl/galaxy-x"> <FaGithub className='footer-icon right' /> </a> 
                    <a href="https://twitter.com/Stacha_cl">  <FaTwitter className='footer-icon right'/>  </a> 
                    <a href="https://www.linkedin.com/in/stacha-c-a7b3b51bb/">  <FaLinkedin className='footer-icon right'/> </a> 
-                   </div>
                 </div>
-                
-            <Form />
             <Footer/>
         </div>
     )
