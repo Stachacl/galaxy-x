c349293cedba6ace1c2e4285ec8e6bea84fbbf21
Stachacl <stacha.l@icloud.com>
2022-11-06 09:56:25 +1100

    contact page - stacha clemens social media links added

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,12 +3,24 @@ import Footer from '../components/Footer'
 import HeroImage from '../components/HeroImage'
 import Navbar from '../components/Navbar'
 import Form from '../components/Form'
+import { FaTwitter, FaGithub, FaLinkedin, FaPhone, FaSearchLocation, FaMailBulk } from 'react-icons/fa'
 
 const Contact = () => {
     return (
         <div>
             <Navbar />
             <HeroImage heading='CONTACT GALAXY X' text='This website was created by Stacha Clemens. If you wish to contact her - please connect via Twitter' />
+            <div>
+                 <p> This website was created by Stacha Clemens. If you wish to contact her - please connect via social media. 
+                 </p>
+        
+                 <div className='container center footer-icon'> 
+                   <a href=" https://github.com/Stachacl/galaxy-x"> <FaGithub className='footer-icon right' /> </a> 
+                   <a href="https://twitter.com/Stacha_cl">  <FaTwitter className='footer-icon right'/>  </a> 
+                   <a href="https://www.linkedin.com/in/stacha-c-a7b3b51bb/">  <FaLinkedin className='footer-icon right'/> </a> 
+                   </div>
+                </div>
+                
             <Form />
             <Footer/>
         </div>
